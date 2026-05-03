3246566f614a1bb92262131e41e8baa7b2ffd0bf
Roy Gitahi <roygitahi0@gmail.com>
2026-05-03 23:00:10 +0300

    Update Article.jsx

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 function Article({title, date = "January 1, 1970", preview, minutes}) {
     const makeEmojiList = (minutes) => {
-        const interval = minutes < 30 ?5 : 10;
-        const emoji = minutes < 30 ?"☕️" :"🍱";
+        const interval = minutes < 30 ? 5 : 10;
+        const emoji = minutes < 30 ? "☕️" : "🍱";
 
         let emojis = "";
-        for (let i=0; i<minutes; i+=interval) {
+        for (let i = 0; i < minutes; i += interval) {
             emojis += emoji;
         }
         return emojis;
     }
+    
     return (
         <article>
             <h3>{title}</h3>
@@ -20,3 +21,6 @@ function Article({title, date = "January 1, 1970", preview, minutes}) {
             <p>{preview}</p>
         </article>
     );
+}
+
+export default Article;
